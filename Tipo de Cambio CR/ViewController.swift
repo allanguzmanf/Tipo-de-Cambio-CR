0d1c3c50f9d69cf8d0bafe338cf96c99a5395bb3
Allan Guzman <alla.gf95@gmail.com>
2020-05-22 03:36:55 -0600

    Initial Commit

---
+++ Tipo de Cambio CR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipo de Cambio CR
+//
+//  Created by Allan Guzman on 5/22/20.
+//  Copyright © 2020 Allan Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
